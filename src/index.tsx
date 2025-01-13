62c8f8387d3927d6fe8351035ff20a90bf4ca778
Shekhar S <shekharshashank97@gmail.com>
2025-01-13 17:41:31 +0530

    Chore:
    Code tweaks and updation

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
-import './index.css'
-import App from './App.tsx'
+import { render } from "solid-js/web";
+import "./index.css";
+import App from "./App.tsx";
+import { Router } from "@solidjs/router";
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
-render(() => <App />, root!)
+render(() => <Router root={App}></Router>, root!);
