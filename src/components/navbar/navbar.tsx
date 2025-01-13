a4096c79846319787e0da91a8a89b93673eed1aa
Shekhar S <shekharshashank97@gmail.com>
2025-01-13 17:41:12 +0530

    Feat:
    Components updation

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,14 +3,15 @@ import { Component, JSXElement } from "solid-js";
 
 // interface props
 interface NavbarProps {
+    id?: string;
     style?: string;
     children?: JSXElement;
 }
 
 // navbar functional component
 const Navbar: Component<NavbarProps> = (props: NavbarProps) => {
-    const { style, children } = props;
-    return <header class={style}>{children} </header>;
+    const { style, children, id } = props;
+    return <header class={style} id={id}>{children} </header>;
 };
 
 export default Navbar;
