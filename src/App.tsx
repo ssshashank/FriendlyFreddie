02d00aef14cbd625184f539e829dc386bccb78fb
Shekhar S <shekharshashank97@gmail.com>
2025-01-15 16:26:40 +0530

    animation added

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Pills from "./components/pills/pills";
 import ffLogo from "./assets/ff.svg";
 import ffLightLogo from "./assets/ffLight.svg";
 import LenisScroll from "./components/lenisScroll/lenisScroll";
+import SampleImg from "./assets/sample.jpg";
 import {
   copyright,
   country,
@@ -18,11 +19,25 @@ import {
   year,
 } from "./constants/appConstant";
 import { MoveUp, Send } from "lucide-solid";
+import { animate, transform } from "motion";
 
 function App() {
   let lenis: Lenis;
+  let animateImage: any;
   const [activeSection, setActiveSection] = createSignal("");
 
+  onMount(() => {
+    animate(
+      animateImage,
+      {
+        transform: ["scaleX(0)", "scaleX(1)"],
+      },
+      {
+        duration: 1, // Adjust to your liking
+        ease: "easeInOut", // "ease-in-out" / "ease-out" / etc.
+      },
+    );
+  });
   onMount(() => {
     // initialize Lenis once when component mounts
     lenis = new Lenis({
@@ -98,7 +113,7 @@ function App() {
                 easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
               });
             }}
-            class="text-3xl h-[65px]  flex items-center justify-center bg-[#222222] font-bold rounded-md  text-white text-center p-[20px]"
+            class="text-3xl h-[65px]  flex items-center justify-center bg-[#222222] font-bold rounded-md  text-white text-center p-[20px] hover:bg-[#333333] duration-300"
           >
             <img
               src={ffLightLogo}
@@ -112,7 +127,7 @@ function App() {
                 <A
                   onClick={handleClick(item?.href)}
                   href={item?.href}
-                  class={`text-sm tracking-wider duration-300 hover:px-6 font-semibold uppercase border py-3 px-4  rounded-md text-center ${
+                  class={`text-sm tracking-wider duration-300 hover:bg-[#333333] font-semibold uppercase border py-3 px-4  rounded-md text-center ${
                     isActive(item.href) ? "border-[#FF4E21]" : "border-gray-500"
                   }`}
                 >
@@ -121,50 +136,43 @@ function App() {
               )}
             </For>
           </span>
-          <span class="bg-[#FF4E21] rounded-md text-white h-[65px] flex gap-3 items-center justify-center px-3">
-            <Button style="text-sm text-center rounded-md  font-semibold tracking-wider uppercase flex items-center justify-center py-3 px-4    text-white duration-300">
+          <span class="bg-[#FF4E21] rounded-md text-white h-[65px] flex gap-3 items-center justify-center px-3 hover:bg-white hover:text-black duration-300">
+            <Button style="text-sm text-center rounded-md  font-semibold tracking-wider uppercase flex items-center justify-center py-3 px-4    ">
               Contact us
             </Button>
           </span>
         </nav>
       </Navbar>
-      <section class="my-3 w-[85%] mx-auto  flex justify-between items-center">
-        <img
-          src={ffLogo}
-          class="h-[100px] max-h-[100px]"
-          alt="friedly_freddie_logo"
-        />
-
-        <div class="flex items-end flex-col ">
-          <span class="flex">
-            <p class="text-[12px] font-medium uppercase">{day},</p>
-            &nbsp;
-            <p class="text-[12px] font-medium">{date},</p>
-            &nbsp;
-            <p class="text-[12px]  font-medium">{time}</p>
-          </span>
-          <Pills style="text-[12px] uppercase font-medium border px-3 py-1 rounded-3xl bg-gradient-to-r from-[#FF4E21] to-black text-white">
-            {country}
-          </Pills>
-        </div>
-      </section>
+      <section class="my-3 w-[85%] mx-auto  flex justify-between items-center"></section>
       <section class="h-[100vh]" id="home">
-        <div class="w-[85%] mx-auto">
-          <div class="flex items-center justify-start">
-            <div>
-              <h1 class="text-[9rem] font-sans  leading-tight">
-                Let’s &nbsp;&nbsp;Redefine
-              </h1>
-              <h1 class="text-[9rem]  font-sans text-center leading-tight">
-                What’s &nbsp;&nbsp;Possible
-              </h1>
-            </div>
-            <div class="flex-1">
+        <div class="w-[90%] mx-auto">
+          <div class="flex  justify-start">
+            <div class="w-[50%] ">
               <img
-                src={
-                  "https://images.unsplash.com/photo-1722942847058-44fdd681e281?q=80&w=3687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                }
+                src={ffLogo}
                 class="h-[100px] max-h-[100px]"
+                alt="friedly_freddie_logo"
+              />
+              <div class="mb-10 mt-[300px]">
+                <h1 class="text-[5rem] font-sans font-medium leading-tight">
+                  Let’s <span class="text-[#FF4E21]"> Redefine</span>
+                </h1>
+                <h1 class="text-[5rem]  font-sans font-medium leading-tight">
+                  What’s Possible
+                </h1>
+                <p class="text-lg mt-2 mb-5 text-gray-500 font-normal">
+                  Rebuilding apps and websites that drive real results for your
+                  business<span class="text-xs"> &#x2022;</span>
+                </p>
+              </div>
+            </div>
+            <div
+              ref={animateImage}
+              class=" origin-left flex-1 h-[95vh] rounded-md overflow-hidden "
+            >
+              <img
+                class="w-full object-contain duration-1000"
+                src={SampleImg}
                 alt="friedly_freddie_logo"
               />
             </div>
